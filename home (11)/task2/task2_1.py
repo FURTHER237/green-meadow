9c3c799785f871a04e0e73af868c302a22f8feab
FURTHER237 <daniel.dawson@student.unimelb.edu.au>
2025-04-14 12:07:37 +1000

    task21

--- home (11)/task2/task2_1.py
+++ home (11)/task2/task2_1.py
@@ -1,7 +1,39 @@
 import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+import string
+import nltk 
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 def task2_1():
-    df = pd.read_csv("accident_dataset.csv")
-    cleaned_descriptions = preprocess_text(df['DCA_DESC'])
+    df = pd.read_csv("../accident.csv")
+    stop_words = set(stopwords.words('english'))
+    
+    # Clean and process text
+    cleaned_descriptions = (
+        df['DCA_DESC']
+        .dropna()
+        .str.lower()
+        .str.translate(str.maketrans('', '', string.punctuation))
+        .apply(lambda x: ' '.join(word for word in x.split() if word not in stop_words))
+    )
+    
+    # Get word frequencies
+    all_words = ' '.join(cleaned_descriptions).split()
+    word_freq = Counter(all_words)
+    top_20_words = dict(word_freq.most_common(20))
+    
+    # Generate word cloud
+    wordcloud = WordCloud(width=800, height=400, background_color='white')
+    wordcloud.generate_from_frequencies(top_20_words)
+    
+    # Save the word cloud
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig('task2_1_word_cloud.png', dpi=300, bbox_inches='tight')
+    plt.close()
     
     return
