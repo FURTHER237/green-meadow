ec4cb96a9f56991a1d6b1d21309e1deb1c57adb5
Xinyi-K <xinyikong@student.unimelb.edu.au>
2025-05-14 17:22:41 +1000

    n

--- home (11)/task5/main.py
+++ home (11)/task5/main.py
@@ -4,7 +4,6 @@ from lightgbm_model import train_lightgbm
 from mlp_model import train_mlp
 
 def main(model_type):
-    # 选择不同的数据源
     if model_type == "lightgbm":
         df = pd.read_csv("../merged_cleaned.csv")
     elif model_type == "mlp":
@@ -13,22 +12,18 @@ def main(model_type):
     else:
         raise ValueError("Unsupported model type. Use 'lightgbm' or 'mlp'.")
 
-    # 提取特征和标签
     X = df.drop("SEVERITY", axis=1)
     y = df["SEVERITY"]
 
-    # LightGBM 特殊处理类别特征
     if model_type == "lightgbm":
         for col in X.select_dtypes(include='object').columns:
             X[col] = X[col].astype('category')
 
-    # 选择对应训练函数
     model_func = {
         "lightgbm": train_lightgbm,
         "mlp": train_mlp
     }[model_type]
 
-    # 模型训练与评估
     model, report, acc, f1 = model_func(X, y)
     print("Model:", model_type)
     print(report)
